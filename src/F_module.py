1a1325e1c2a9999ca7ca5d5072ded7bc2b5b324f
sunwh1521 <2904692323@qq.com>
2024-10-15 22:52:03 +0800

    完善F因子计算公式

--- src/F_module.py
+++ src/F_module.py
@@ -150,3 +150,69 @@ class f_SP_class(object):
         Chemical Engineering Science, 2009, 64(14): 3290-3300.
         """
         return 14.5*Re**(-0.135)
+    
+    def MM_cal(self, Re, ang_corrugated, projection_coefficient):
+        """Muley和Manglik提出的板式换热器摩擦因子经验公式
+        适用范围： ang_corrugated:30°~80°, Re:1000~40000
+        :Re:雷诺数 1000~40000
+        :ang_corrugated: 波纹角° 范围：30°~80°
+        :projection_coefficient：面积投影系数 范围：不确定
+        :returns: 摩擦因子
+        
+        来源：[1] Muley, A., Manglik, P.M., Experimental study of turbulent flow transfer and pressure drop in a plate heat exchanger with Chevron plates [J]. Journal of Heat Transfer. 1999.121 (1): 110-117.
+        """
+        return (2.917-0.1277*ang_corrugated+2.016e-3*ang_corrugated**2)*(5.474-19.02*projection_coefficient+18.93*projection_coefficient**2-5.341*projection_coefficient**3)*Re**{-0.2+0.0577*math.sin[(math.pi*ang_corrugated)/45+2.1]}
+
+    def Talik_cal(self, Re):
+        """Talik 提出的板式换热器摩擦因子计算公式
+        适用范围：1.流动工质是水 2.Re：1450～11460
+        
+        :Re: 雷诺数 1450～11460
+        :returns: 摩擦因子
+        来源：[1] Pages 1033-1044,Talik, A. C., Fletcher, L. S., Anand. N. K., Swanson, L. M., Heat transfer and pressure drop characteristics of a plate heat exchanger [A]. Proceedings of the ASME/JSME Thermal Engineering Conference, ASME, New York, 1995,4. 321-329.
+        """
+        return 0.3323* Re**(-0.042)
+    
+    def Pantzali_cal(self, Re):
+        """Pantzali等人提出的板式换热器摩擦因子计算公式
+        适用范围：1.纳米流体是冷却剂 2.CuO/water(6.0 vol.%) 3.水作为冷却剂 4.层流/紊流
+
+        :Re: 雷诺数 
+        :returns: 摩擦因子
+
+        来源：[1] Pantzali M N, Mouza A A, Paras S V. Investigating the efficacy of nanofluids as coolants in plate heat exchangers (PHE)[J]. Chemical Engineering Science, 2009, 64(14): 3290-3300.
+        """
+        return 14.5* Re**(-0.135)
+    
+    def R_D_V_cal(self, Re):
+        """ Ray D R,Das D K,Vajjha R S提出的板式换热器摩擦因子计算公式
+        适用范围：1. Al2O3/EG:Water(~5 vol.%) 2. 4<Pr<27 3. 120<Re<1000
+
+        :Re: 雷诺数 
+        :returns: 摩擦因子
+
+        来源：[1] Ray D R, Das D K, Vajjha R S. Experimental and numerical investigations of nanofluids performance in a compact minichannel plate heat exchanger[J]. International Journal of Heat and Mass Transfer, 2014, 71: 732-746.
+        """
+        return 13.64*Re**(-0.2719)
+    
+
+class f_TP_class(object):
+    """双相波纹板换摩擦因子计算类"""
+    def __init__(self):
+        """TODO: to be defined. """
+        self.logger = logging.getLogger(__name__)  # 调用_setup_logger方法设置日志记录器
+
+    def Behrozifard_cal(self, Kp, Re, m):
+        """Behrozifard 等人提出的纳米流体与水的板式换热器的摩擦因子计算公式
+        适用范围：1.Re<2300 2.换热器采用M6 Alfa Laval 垫片型板式换热器 3.换热介质为纳米流体和水
+        
+        :Re: 雷诺数 <2300
+        :returns: 摩擦因子
+        
+        来源：[1] A. Behrozifard, Hamid Reza Goshayeshi, Iman Zahmatkesh, Issa Chaer, Soheil Salahshour, D. Toghraie,Experimental optimization of the performance of a plate heat exchanger with Graphene oxide/water and Al₂O₃/water nanofluids,Case Studies in Thermal Engineering, 59,2024.
+        """
+        return Kp/(Re**m)
+    
+f_TP = f_TP_class()
+a=f_TP.Behrozifard_cal(1.5, 1000,1)
+print(a)
