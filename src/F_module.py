81aa1a13b0ac203d9a10474d3b9b4f89aa6a1acd
yanming-ubuntu <lxb-yanming@foxmail.com>
2024-10-16 09:50:11 +0800

    完善F因子计算公式

--- src/F_module.py
+++ src/F_module.py
@@ -27,7 +27,7 @@
 """
 
 import logging
-
+import sys
 
 class f_SP_class(object):
     """单相波纹板换摩擦因子计算类"""
@@ -45,7 +45,11 @@ class f_SP_class(object):
         
         来源：[1] Saturated flow boiling heat transfer and pressure drop of refrigerant R-410A in a vertical plate heat exchanger[J/OL]. International Journal of Heat and Mass Transfer, 2002, 45(5): 1033-1044. DOI:10.1016/S0017-9310(01)00219-8.
         """
-        return 61000 * Re**(-1.25)
+        if 10< Re < 400:
+            return 61000 * Re**(-1.25)
+        else:
+            self.logger.error("YY_Hsich 仅适用于Re：10～400 立式板式换热器，R410A 制冷剂")
+            sys.exit()
 
     def LJY_cal(self, Re_eq, Re_f):
         """林俊宇提出的高温热泵板换 R245fa 摩擦因子计算公式
@@ -59,59 +63,76 @@ class f_SP_class(object):
         """
         return 24502 * Re_eq**(-0.8521) * Re_f**(-0.1856)
     
-    def Gulenoglu_C_1_cal(self, Re,fai):
+    def Gulenoglu_C_1_cal(self, Re,phi):
         """
         Gulenoglu 等人三种垫片板式换热器中使用的的摩擦因子计算公式1（φ=1.17与2相同）
 
         适用范围：波纹角30°，工质为水
         :Re: 雷诺数 300～5000
-        :fai: 放大系数
+        :phi: 放大系数
         :returns: 摩擦因子
 
         来源：[25] Gulenoglu C, Akturk F, Aradag S, et al. 
             Experimental comparison of performances of three different plates for gasketed plate heat exchangers[J]. 
             International Journal of Thermal Sciences, 2014, 75: 249-256.
         """
-        if fai==1.17:
-            F=259.9*Re**(-0.9227)+1.246
-        elif fai==1.288:
-            F=0.00374*Re**(0.5981)+0.9132
+        if 300 < Re < 5000:
+            if phi==1.17:
+                F=259.9*Re**(-0.9227)+1.246
+            elif phi==1.288:
+                F=0.00374*Re**(0.5981)+0.9132
+        else:
+            self.logger.error("Gulenoglu_C_1 仅适用于Re：300～5000 波纹角30°，工质为水")
+            sys.exit()
         return F
     
-    def Gulenoglu_C_2_cal(self, Re,fai):
+    def Gulenoglu_C_2_cal(self, Re,phi):
         """
         Gulenoglu 等人三种垫片板式换热器中使用的摩擦因子计算公式2（φ=1.288与1相同）
 
         适用范围：波纹角30°，工质为水
         :Re: 雷诺数 300～5000
-        :fai: 放大系数
+        :phi: 放大系数
         :returns: 摩擦因子
 
         来源：[25] Gulenoglu C, Akturk F, Aradag S, et al. 
             Experimental comparison of performances of three different plates for gasketed plate heat exchangers[J]. 
             International Journal of Thermal Sciences, 2014, 75: 249-256.
         """
-        if fai==1.288:
-            F=0.00374*Re**(0.5981)+0.9132
-        elif fai==1.17:
-            F=1371*Re**(-1.146)+1.139
+        if 300 < Re < 5000:
+            if phi==1.288:
+                F=0.00374*Re**(0.5981)+0.9132
+            elif phi==1.17:
+                F=1371*Re**(-1.146)+1.139
+        else:
+            self.logger.error("Gulenoglu_C_2 仅适用于Re：300～5000 波纹角30°，工质为水")
+            sys.exit()
         return F
     
-    def Alklaibi_cal(self, Re, fai):
+    def Alklaibi_cal(self, Re, phi):
         """
         Alklaibi 等人提出的垫片板式换热器中使用的摩擦因子计算公式
         适用范围：Re：300~1000， φ 0~0.3%, β 30°， 工作介质制冷剂（MWCNT/水纳米流体）/水的板式换热器
 
         :Re: 雷诺数   300~1000
-        :fai: 颗粒体积浓度
+        :phi: 颗粒体积浓度
 
         来源：[8] A.M. Alklaibi, L. Syam Sundar, Kotturu V.V. Chandra Mouli,
         Experimental investigation on the performance of hybrid Fe3O4 coated MWCNT/Water nanofluid as a coolant of a Plate heat exchanger,
         International Journal of Thermal Sciences, 171,2022.
         """
-        return (69.96/Re)/(1+fai)**(-0.24)
+        if 300<Re<1000:
+            if 0<phi<0.3:
+                return (69.96/Re)/(1+phi)**(-0.24)
+            else:
+                self.logger.error("Alklaibi 仅适用于φ：0~0.3%")
+                sys.exit()
+        else:
+            self.logger.error("Alklaibi 仅适用于Re：300~1000")
+            sys.exit()
+        
     
-    def He_Qing_Qiong_cal(self,Re,F):
+    def He_Qing_Qiong_cal(self,Re):
         """
         He-Qing-Qiong 等人提出的摩擦因子计算公式
         适用范围：波纹夹角45°，横波波距30mm，纵波波距16mm，纵波波高1.5mm，工质为水、油
@@ -124,21 +145,28 @@ class f_SP_class(object):
             F=27.487*Re**(-0.5785)
         elif Re<20000 and Re>2000:
             F=4.9772*Re**(-0.31550)
+        else:
+            self.logger.error("He-Qing-Qiong 仅适用于Re：50～20000")
+            sys.exit()
         return F
 
-    def Amooie_FMM_cal(self,Re,fai):
+    def Amooie_FMM_cal(self,Re,phi):
         """
         Amooie, F.M.M. 等人摩擦因子计算公式
         适用范围：Re：0.8~2220，流动工质是水的水平波纹板式换热器
 
         :Re: 雷诺数
-        :fai: 未知
+        :phi: 未知
 
         来源：[18] Amooie, F.M.M., Flow distribution in plate heat exchanger [D]. PhD Thesis, University of Bradford, UK, 1997.
         """
-        return fai*(26*fai/Re+0.16)
+        if 0.8<Re<1000:
+            return phi*(26*phi/Re+0.16)
+        else:
+            self.logger.error("Anooie_FMM 仅适用于Re：0.8~2220")
+            sys.exit()
     
-    def Pantzali_MN_cal(self,Re,fai):
+    def Pantzali_MN_cal(self,Re):
         """
         Pantzali, M.N. 等人摩擦因子计算公式
         适用范围：纳米流体作为冷却剂在板式换热器，
